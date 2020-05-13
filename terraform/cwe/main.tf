39638d6a5646ca65b9ad5f09452d7b5f80ce1cbe
Michael Schappacher <michael.schappacher@gmail.com>
2020-05-12 20:52:20 -0400

    Feature: Refactored to match Cross Region Pattern

---
+++ terraform/cwe/main.tf
@@ -0,0 +1,25 @@
+module "cwe" {
+  source      = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe?ref=v0.6.0"
+  name        = "DetectCloudwatchAlarmsDeleted"
+  description = "Rule to check when Cloudwatch Alarms are Deleted"
+
+  event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.monitoring"
+  ],
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "detail": {
+    "eventSource": [
+      "monitoring.amazonaws.com"
+    ],
+    "eventName": [
+      "DeleteAlarms"
+    ]
+  }
+}
+PATTERN
+
+}
---
+++ terraform/sqs_lambda/variables.tf
@@ -0,0 +1,19 @@
+variable "cloudwatch_event_rule_id" {
+  description = "Easy name of CWE"
+  type        = string
+}
+
+variable "cloudwatch_event_rule_arn" {
+  description = "Full arn of CWE"
+  type        = string
+}
+
+variable "sns_topic_arn" {
+  description = "SNS topic arn of central or local sns topic"
+  type        = string
+}
+
+variable "reflex_kms_key_id" {
+  description = "KMS Key Id for common reflex usage."
+  type        = string
+}
