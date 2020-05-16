4ad4f392d35e80997659184ee161d9f8e97b27ab
Richard Julian <richard@rjulian.net>
2020-05-16 09:31:54 -0700

    Remove detect from names

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -4,7 +4,7 @@ module "sqs_lambda" {
 
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
-  function_name             = "DetectCloudwatchAlarmsDeleted"
+  function_name             = "CloudwatchAlarmsDeleted"
   source_code_dir           = "${path.module}/../../source"
   handler                   = "reflex_aws_cloudwatch_alarms_deleted.lambda_handler"
   lambda_runtime            = "python3.7"
@@ -12,10 +12,10 @@ module "sqs_lambda" {
     SNS_TOPIC = var.sns_topic_arn
   }
 
-  queue_name    = "DetectCloudwatchAlarmsDeleted"
+  queue_name    = "CloudwatchAlarmsDeleted"
   delay_seconds = 0
 
-  target_id = "DetectCloudwatchAlarmsDeleted"
+  target_id = "CloudwatchAlarmsDeleted"
 
   sns_topic_arn  = var.sns_topic_arn
   sqs_kms_key_id = var.reflex_kms_key_id
